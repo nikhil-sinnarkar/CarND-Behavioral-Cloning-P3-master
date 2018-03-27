92362f4480a22914d1e181fd388cd56608e84ae6
nikhil-sinnarkar <35420772+nikhil-sinnarkar@users.noreply.github.com>
2018-03-27 17:20:49 +0530

    Add files via upload

---
+++ generator_example.py
@@ -0,0 +1,43 @@
+import numpy as np
+import csv
+import sklearn
+import cv2
+
+X_train = []
+Y_train = []
+csv_data = []
+
+# read the data in csv file
+with open('./driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		csv_data.append(line)
+	
+def generator(csv_data, batch_size = 32):
+	data_length = len(csv_data)
+	sklearn.utils.shuffle(csv_data)
+	for offset in range(0, data_length, batch_size):
+		# split the data into batches
+		batch_data = csv_data[offset:offset+batch_size]
+		# list to store images and steering angle for each batch
+		images = []
+		steering = []
+		
+		for data in batch_data:
+			image_path = data[0]
+			center_image = cv2.imread(image_path)
+			center_angle = data[3]
+			images.append(center_image)
+			steering.append(center_angle)
+		
+		X_train = np.array(images)
+		Y_train = np.array(steering)
+		yield sklearn.utils.shuffle(X_train, Y_train)
+		
+
+gen_instance = generator(csv_data)
+		
+while(input()=='y'):
+	X_train_batch, Y_train_batch = next(gen_instance)
+	print(X_train_batch.shape)
+	# cv2.waitKey(0)
